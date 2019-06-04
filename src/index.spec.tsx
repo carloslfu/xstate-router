e39a283113a9f010c03fff3850f13234ed2f8cde
Carlos Galarza <carloslfu@gmail.com>
2019-06-03 20:26:06 -0500

    update deps

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { render, fireEvent, cleanup } from 'react-testing-library'
+import { render, fireEvent, cleanup } from '@testing-library/react'
 import { createMemoryHistory } from 'history'
 import { MachineOptions } from 'xstate'
 import { toStatePaths } from 'xstate/lib/utils'
